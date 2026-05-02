5d99de4ca48bc6cf3ddf80e3a14002b889aac661
Oscar <oscar@quntem.co.uk>
2026-05-02 10:26:22 +0100

    fix logo (hopefully) and update grid portal

---
+++ files/scripts/install-branding-assets.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo.svg /usr/share/icons/hicolor/scalable/apps/thetaos-logo.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo.svg /usr/share/icons/hicolor/scalable/apps/thetaos-logo-text.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo-dark.svg /usr/share/icons/hicolor/scalable/apps/thetaos-logo-dark.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo-dark.svg /usr/share/icons/hicolor/scalable/apps/thetaos-logo-text-dark.svg
+
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo.svg /usr/share/icons/hicolor/scalable/apps/fedora-logo-icon.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo.svg /usr/share/icons/hicolor/scalable/apps/fedora-logo-icon-text.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo-dark.svg /usr/share/icons/hicolor/scalable/apps/fedora-logo-icon-dark.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo-dark.svg /usr/share/icons/hicolor/scalable/apps/fedora-logo-icon-text-dark.svg
+
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo.svg /usr/share/pixmaps/thetaos-logo.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo.svg /usr/share/pixmaps/thetaos-logo-text.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo-dark.svg /usr/share/pixmaps/thetaos-logo-dark.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo-dark.svg /usr/share/pixmaps/thetaos-logo-text-dark.svg
+
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo.svg /usr/share/pixmaps/fedora-logo-icon.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo.svg /usr/share/pixmaps/fedora-logo-icon-text.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo-dark.svg /usr/share/pixmaps/fedora-logo-icon-dark.svg
+install -Dm0644 /usr/share/thetaos/branding/thetaos-logo-dark.svg /usr/share/pixmaps/fedora-logo-icon-text-dark.svg
+
+install -Dm0644 /usr/share/thetaos/branding/fedora_logo_med.png /usr/share/pixmaps/fedora_logo_med.png
+install -Dm0644 /usr/share/thetaos/branding/fedora_whitelogo_med.png /usr/share/pixmaps/fedora_whitelogo_med.png
+install -Dm0644 /usr/share/thetaos/branding/fedora_logo_med.png /usr/share/pixmaps/fedora-logo.png
+install -Dm0644 /usr/share/thetaos/branding/fedora_whitelogo_med.png /usr/share/pixmaps/system-logo-white.png
